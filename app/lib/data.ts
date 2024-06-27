c234f495525e00120a3eeefd749f4a97063798ed
smilen-l <slyubenov@dermatic.health>
2024-06-27 16:55:48 -0400

    fix: font

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,12 @@ export async function fetchRevenue() {
 
     // console.log('Data fetch completed after 3 seconds.');
 
-    return data.rows;
+    // const data = await Promise.resolve([{
+    //   month: "string",
+    //   revenue: 0,
+    // }])
+    
+    return data;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { lusitana } from './ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
